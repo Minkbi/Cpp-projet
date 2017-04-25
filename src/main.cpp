44eca066d096f2fe0dd70f5c44e5bbef0d9abfb1
Glen Queguiner <glen.queguiner@ensimag.grenoble-inp.fr>
2017-04-25 15:51:16 +0200

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,28 @@
+#include <cstdio>
+#include <cstdlib>
+#include "GerstnerWaveModel.h"
+
 int main(int argc, char* argv[])
 {
+  FILE * file;
+  file = fopen ("aaa","w");
+  if (file!=NULL)
+  {
+    double dir[2];
+    dir[0] = 1;
+    dir[1] = 0;
+    GerstnerWaveModel gWave(dir,8,9,10);
+    GerstnerWave wave(dir, 11,12,13);
+    gWave.addWave(wave);
+    for (int i = 0 ; i < 10; i++) {
+      for (int j = 0 ; j < 10; j++) {
+	char ligne[1000];	
+	sprintf(ligne,"%f %f %f\n", (float)i,(float)j,gWave(i,j,6));
+	printf("%s",ligne);
+        fputs(ligne,file);
+      }
+    }
+    fclose (file);
+  }
+  return 0;
 }
